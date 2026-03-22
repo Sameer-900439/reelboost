66e07b5ddfe0074f5f80fab9a19c17c9025f06b8
Sameer-900439 <samsantoshgupta@gmail.com>
2026-03-22 18:30:46 +0530

    Fix: Add 5s timeout to Nodemailer to prevent infinite hanging

--- services/emailService.js
+++ services/emailService.js
@@ -15,6 +15,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  connectionTimeout: 5000, // 5 seconds
+  greetingTimeout: 5000,   // 5 seconds
+  socketTimeout: 5000      // 5 seconds
 });
 
 // Verify SMTP connection on startup
